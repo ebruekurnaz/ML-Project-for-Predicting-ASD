4503b146d3258ce6574ec7f69f108748530d1102
Arda Ekinci <arda@chainsage.com>
2019-05-16 23:34:45 +0300

    add train and test data

--- classification.py
+++ classification.py
@@ -2,11 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+import csv
 
 
-def load_data(file_name):
+def load_train_data():
     # Read data
-    train_data = pd.read_csv("data/{}.csv".format(file_name))
+    train_data = pd.read_csv("data/train.csv")
 
     # Remove zeros from data
     train_data = train_data.loc[:, (train_data != 0).any(axis=0)]
@@ -20,15 +21,35 @@ def load_data(file_name):
 
     # Remove last colum
     train_x = train_data[:,0:col_size]
-    print(train_x)
-    print(train_y)
+
     return train_x, train_y
 
-train_x, train_y = load_data("train")
-test_x, test_y = load_data("test")
 
+def load_test_data():
+    # Read data
+    test_data = pd.read_csv("data/test.csv")
 
+    # Remove zeros from data
+    test_data = test_data.loc[:, (test_data != 0).any(axis=0)]
+
+    return test_data.values
+
+# Load Datas
+train_x, train_y = load_train_data()
+test_x = load_test_data()
+
+# Apply SVC
 clf = SVC(kernel='linear')
 clf.fit(train_x, train_y)
-pred_y = clf.predict(train_x)
-print(accuracy_score(train_y,pred_y))
+pred_y = clf.predict(test_x)
+
+# Write to submission file
+submission_file = [["ID", "Predicted"]]
+for i, prediction in enumerate(pred_y):
+    submission_file.append([i + 1, int(prediction)])
+
+with open('submission.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(submission_file)
+
+# print(accuracy_score(train_y,pred_y))
