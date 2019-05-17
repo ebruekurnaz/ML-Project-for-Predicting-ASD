616fda56238941392c70ab47086fd4d6ed831581
eylul kurnaz <ebrukurnaz@anadolusigorta.com.tr>
2019-05-17 18:50:21 +0300

    Random Forest and K means algorithms are implemented

--- classification.py
+++ classification.py
@@ -3,6 +3,10 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 import csv
 
 
@@ -11,7 +15,7 @@ def load_train_data():
     train_data = pd.read_csv("data/train.csv")
 
     # Remove zeros from data
-    #train_data = train_data.loc[:, (train_data != 0).any(axis=0)]
+    train_data = train_data.loc[:, (train_data != 0).any(axis=0)]
 
     # Change data to numpy array
     train_data = train_data.values
@@ -31,11 +35,22 @@ def load_test_data():
     test_data = pd.read_csv("data/test.csv")
 
     # Remove zeros from data
-    #test_data = test_data.loc[:, (test_data != 0).any(axis=0)]
+    test_data = test_data.loc[:, (test_data != 0).any(axis=0)]
 
     return test_data.values
 
-def train_using_gini(X_train, X_test, y_train): 
+def train_random_forest(X_train, y_train):
+
+	# Instantiate rf
+    rf = RandomForestRegressor(n_estimators=25,
+            random_state=2)
+            
+	# Fit rf to the training set    
+    rf.fit(X_train, y_train) 
+
+    return rf
+
+def train_using_gini(X_train, y_train): 
   
     # Creating the classifier object 
     clf_gini = DecisionTreeClassifier(criterion = "gini", 
@@ -44,7 +59,6 @@ def train_using_gini(X_train, X_test, y_train):
     # Performing training 
     clf_gini.fit(X_train, y_train) 
     return clf_gini 
-
 
 # Load Datas
 train_x, train_y = load_train_data()
@@ -55,8 +69,17 @@ test_x = load_test_data()
 #clf.fit(train_x, train_y)
 #pred_y = clf.predict(test_x)
 
-clf_gini = train_using_gini(train_x, test_x,train_y)
-pred_y = clf_gini.predict(test_x)
+
+#Apply Decision Tree
+#clf_gini = train_using_gini(train_x, train_y)
+#pred_y = clf_gini.predict(test_x)
+
+#Apply K means
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(train_x,train_y)
+
+pred_y = kmeans.predict(test_x)
+
 
 # Write to submission file
 submission_file = [["ID", "Predicted"]]
