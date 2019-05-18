192276d8e05fb85a183781fb701e15a93fbc1062
eylul kurnaz <ebrukurnaz@anadolusigorta.com.tr>
2019-05-18 13:28:07 +0300

    5-Fold Cross Validation implemented

--- classification.py
+++ classification.py
@@ -7,8 +7,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA, KernelPCA
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import KFold
 import csv
 
 
@@ -62,6 +62,7 @@ def train_using_gini(X_train, y_train):
     clf_gini.fit(X_train, y_train) 
     return clf_gini 
 
+
 def get_pca_results(X, Y, x_test, y_test):
     results = []
     for i in range(1,90):
@@ -69,7 +70,7 @@ def get_pca_results(X, Y, x_test, y_test):
         pca = KernelPCA(n_components=i, kernel='sigmoid')
         x_train = pca.fit_transform(X)
 
-        clf = RandomForestClassifier(max_depth=2, random_state=0)  
+        clf = train_using_gini(X,Y) 
         clf.fit(x_train, Y)
 
         # clf = SVC(kernel='sigmoid').fit(train_x,train_y)
@@ -87,11 +88,15 @@ def get_pca_results(X, Y, x_test, y_test):
 
 
 # Load Datas
-train_x, train_y = load_train_data()
+X, y = load_train_data()
 test_x = load_test_data()
 
+#Apply 5-Fold Cross Validation
+kf = KFold(n_splits = 5)
+
+
 #To generate test set randomly and split it from the data
-X_train, X_test, y_train, y_test = train_test_split(train_x, train_y, test_size=0.2) 
+#X_train, X_test, y_train, y_test = train_test_split(train_x, train_y, test_size=0.2) 
 
 # Apply SVC
 #clf = SVC(kernel='rbf', gamma = 100, C = 100).fit(train_x,train_y)
@@ -109,30 +114,40 @@ X_train, X_test, y_train, y_test = train_test_split(train_x, train_y, test_size=
 # pred_y = kmeans.predict(test_x)
 
 
-results = get_pca_results(X_train, y_train, X_test, y_test)
-print(results)
+overall_accuracy = 0;
 
-pca = KernelPCA(n_components=results[0][0], kernel='sigmoid')
-# pca = PCA(n_components=results[0][0])
-x_train = pca.fit_transform(X_train)
-
-clf = train_using_gini(x_train, y_train)
-
-x_test = pca.transform(X_test)
-print(len(x_test[0]))
-x_kaggle = pca.transform(test_x)
-test_pred = clf.predict(x_kaggle)
-y_pred = clf.predict(x_test)
-print('Accuracy {}'.format(accuracy_score(y_test, y_pred)))  
+for train_index, test_index in kf.split(X):
+	X_train, X_test = X[train_index], X[test_index]
+	y_train, y_test = y[train_index], y[test_index]
 
 
-# Write to submission file
-submission_file = [["ID", "Predicted"]]
-for i, prediction in enumerate(test_pred):
-    submission_file.append([i + 1, int(prediction)])
+	results = get_pca_results(X_train, y_train, X_test, y_test)
+	#print(results)
 
-with open('submission.csv', 'w') as csvFile:
-    writer = csv.writer(csvFile)
-    writer.writerows(submission_file)
+	pca = KernelPCA(n_components=results[0][0], kernel='sigmoid')
+	# pca = PCA(n_components=results[0][0])
+	x_train = pca.fit_transform(X_train)
 
+	clf = train_using_gini(x_train, y_train)
+
+	x_test = pca.transform(X_test)
+	print(len(x_test[0]))
+	x_kaggle = pca.transform(test_x)
+	test_pred = clf.predict(x_kaggle)
+	y_pred = clf.predict(x_test)
+	print("TEST: ",  test_index)
+	print('Accuracy {}'.format(accuracy_score(y_test, y_pred)))  
+	overall_accuracy += accuracy_score(y_test, y_pred)
+
+	# Write to submission file
+	submission_file = [["ID", "Predicted"]]
+	for i, prediction in enumerate(test_pred):
+	    submission_file.append([i + 1, int(prediction)])
+
+	with open('submission.csv', 'w') as csvFile:
+	    writer = csv.writer(csvFile)
+	    writer.writerows(submission_file)
+
+
+print("Overall Accuracy: " , overall_accuracy/5)
 # print(accuracy_score(train_y,pred_y))
