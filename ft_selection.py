ab64d86fda7ef337442b9766b17cb9c632b27579
eylul kurnaz <ebrukurnaz@anadolusigorta.com.tr>
2019-05-18 22:44:37 +0300

    RFE method added

--- ft_selection.py
+++ ft_selection.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from sklearn.feature_selection import RFE
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 
 data = pd.read_csv("data/train.csv")
 X = data.iloc[:,0:595]  #independent columns
@@ -20,28 +20,27 @@ y = data.iloc[:,-1]    #target column i.e price range
 
 
 #apply SelectKBest class to extract top 10 best features
-bestfeatures = SelectKBest(score_func=chi2, k="all")
-fit = bestfeatures.fit(X,y)
-dfscores = pd.DataFrame(fit.scores_)
-dfcolumns = pd.DataFrame(X.columns)
-#concat two dataframes for better visualization 
-featureScores = pd.concat([dfcolumns,dfscores],axis=1)
-featureScores.columns = ['Specs','Score']  #naming the dataframe columns
-print(featureScores.nlargest(10,'Score'))  #print 10 best features
-ft = featureScores.nlargest(300,'Score')
-indexes = ft.index.values
-values = ft.values   
-print(indexes)
-print(np.sum(values[:,-1]))
+# bestfeatures = SelectKBest(score_func=chi2, k="all")
+# fit = bestfeatures.fit(X,y)
+# dfscores = pd.DataFrame(fit.scores_)
+# dfcolumns = pd.DataFrame(X.columns)
+# #concat two dataframes for better visualization 
+# featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+# featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+# print(featureScores.nlargest(10,'Score'))  #print 10 best features
+# ft = featureScores.nlargest(300,'Score')
+# indexes = ft.index.values
+# values = ft.values   
+# print(indexes)
+# print(np.sum(values[:,-1]))
 
 
 X = X.values
-print(X[:, indexes])
 
-# model = LogisticRegression()
-# # create the RFE model and select 3 attributes
-# rfe = RFE(model, 100)
-# rfe = rfe.fit(X, y)
-# # summarize the selection of the attributes
-# print(rfe.support_)
-# print(rfe.ranking_)
+svm = LinearSVC()
+# create the RFE model for the svm classifier 
+# and select attributes
+rfe = RFE(svm, 10)
+rfe = rfe.fit(X, y)
+# print summaries for the selection of attributes
+print(rfe.ranking_[:10])
